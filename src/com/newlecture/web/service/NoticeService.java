da064bf7edd9bc9d1893eb0e9de70bf0224c1edc
SunghyunKim9744 <dqq9744@naver.com>
2020-12-14 12:41:57 +0900

    12/14 DAO+VIEW

--- src/com/newlecture/web/service/NoticeService.java
+++ src/com/newlecture/web/service/NoticeService.java
@@ -1,211 +1,67 @@
 package com.newlecture.web.service;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
+import com.newlecture.web.dao.NoticeDao;
+import com.newlecture.web.dao.jdbc.JdbcNoticeDao;
 import com.newlecture.web.entity.Notice;
 
 public class NoticeService {
-
-	public List<Notice> getList() {
-		String url = "jdbc:oracle:thin:@hi.namoolab.com:1521/xepdb1";
-		String sql = "SELECT * FROM NOTICE";
-
-		List<Notice> list = new ArrayList<>();
-
-		try {
-			// Driver load
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			// DB 연결
-			Connection con = DriverManager.getConnection(url, "NEWLEC", "11111");
-
-			// DB 실행
-			Statement st = con.createStatement();
-
-			// DB 결과
-			ResultSet rs = st.executeQuery(sql);
-
-//			String[] nicnames= new String[2];
-
-			while (rs.next()) {
-
-				int id = rs.getInt("ID");
-				String title = rs.getNString("TITLE");
-				String writerId = rs.getNString("WRITER_ID");
-				String content = rs.getNString("CONTENT");
-				Date regdate = rs.getDate("REGDATE");
-				int hit = rs.getInt("HIT");
-				String files = rs.getNString("FILES");
-
-				Notice n = new Notice(id, title, writerId, content, regdate, hit, files);
-
-				list.add(n);
-			}
-
-			rs.close();
-			st.close();
-			con.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		return list;
+	
+	private NoticeDao noticeDao;
+	
+	public NoticeService() {
+		
+		noticeDao = new JdbcNoticeDao();
 	}
 
-	public Notice get(int id) {
-
-		Notice n = null;
-
-		String url = "jdbc:oracle:thin:@hi.namoolab.com:1521/xepdb1";
-		String sql = "SELECT * FROM NOTICE WHERE ID=" + id;
-
-		try {
-			// Driver load
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			// DB 연결
-			Connection con = DriverManager.getConnection(url, "NEWLEC", "11111");
-
-			// DB 실행
-			Statement st = con.createStatement();
-
-			// DB 결과
-			ResultSet rs = st.executeQuery(sql);
-
-//			String[] nicnames= new String[2];
-
-			if (rs.next()) {
-
-				// int id = rs.getInt("ID");
-				String title = rs.getNString("TITLE");
-				String writerId = rs.getNString("WRITER_ID");
-				String content = rs.getNString("CONTENT");
-				Date regdate = rs.getDate("REGDATE");
-				int hit = rs.getInt("HIT");
-				String files = rs.getNString("FILES");
-
-				n = new Notice(id, title, writerId, content, regdate, hit, files);
-
-			}
-
-			rs.close();
-			st.close();
-			con.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		return n;
-	}
-
-//	조작된 데이터의 개수
+	
 	public int insert(Notice notice) {
 		
-		int result = 0;
-		
-		String url = "jdbc:oracle:thin:@hi.namoolab.com:1521/xepdb1";
-		String sql = "INSERT INTO NOTICE(TITLE,CONTENT) VALUES(?,?)";
+		return noticeDao.insert(notice);
+	}
+	
+	public List<Notice> getList(){
+		return null;
+	}
+	public List<Notice> getList(int page, int size, String field, String query) {
+		int startIndex = 1+(page-1)*size;
+		int endIndex = page*10;
+		return noticeDao.getList(startIndex, endIndex, field, query);
+	}
 
-		try {
-		
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			Connection con = DriverManager.getConnection(url, "NEWLEC", "11111");
-			
-//			? 값을 꽂아 놓을 수 있게 함 - > PreparedStatement
-			PreparedStatement st = con.prepareStatement(sql);
-			st.setString(1, notice.getTitle());
-			st.setString(2, notice.getContent());
-			
-//			결과가 있을 때만 필요(select 일경우만)
-//			ResultSet rs = st.executeQuery(sql);
-			
-//			insert, update, delete 문장일 때
-			result = st.executeUpdate();
-
-			st.close();
-			con.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+	public int hitUp(int id) {
+		int result =0;
+		Notice notice = noticeDao.get(id);
+		notice.setHit(notice.getHit()+1);
+		result = noticeDao.update(notice);
 		return result;
+	}
+	
+	public int deletaAll(int[] ids) {
+		
+		int result = 0;
+		for(int i=0; i<ids.length; i++) {
+			int id= ids[i];
+			result += noticeDao.delete(id);
+		}
+			
+		return result;
+	}
+	
+	
+	public Notice get(int id) {
 
+		return noticeDao.get(id);
 	}
 
 	public int update(Notice notice) {
-		int result = 0;
 		
-		String url = "jdbc:oracle:thin:@hi.namoolab.com:1521/xepdb1";
-		String sql = "UPDATE NOTICE SET TITLE=?,CONTENT=? WHERE ID=?";
-
-		try {
-		
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			Connection con = DriverManager.getConnection(url, "NEWLEC", "11111");
-			
-//			? 값을 꽂아 놓을 수 있게 함 - > PreparedStatement
-			PreparedStatement st = con.prepareStatement(sql);
-			st.setString(1, notice.getTitle());
-			st.setString(2, notice.getContent());
-			st.setInt(3, notice.getId());
-			
-			result = st.executeUpdate();
-
-			st.close();
-			con.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		return result;
-		
+		return noticeDao.update(notice);
 	}
 	
 	public int delete(int id) {
-		int result = 0;
-		String url = "jdbc:oracle:thin:@hi.namoolab.com:1521/xepdb1";
-		String sql = "DELETE FROM NOTICE WHERE ID=?";
-
-		try {
-		
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			Connection con = DriverManager.getConnection(url, "NEWLEC", "11111");
-			PreparedStatement st = con.prepareStatement(sql);
-			st.setInt(1, id);
-			
-			result = st.executeUpdate();
-
-			st.close();
-			con.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return result;
+	
+		return noticeDao.delete(id);
 	}
 }
